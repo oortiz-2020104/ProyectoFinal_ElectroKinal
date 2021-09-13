382565d39dff77985c30f0c31e31ee9c4bed4304
Javier Linares <jlinares-2020162@kinal.edu.gt>
2021-09-13 17:03:03 -0600

    Se agregó el método eliminar en ClienteDAO

--- ProyectoFinal_ElectroKinal/src/java/modelo/ClienteDAO.java
+++ ProyectoFinal_ElectroKinal/src/java/modelo/ClienteDAO.java
@@ -28,13 +28,13 @@ public class ClienteDAO {
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                Cliente em = new Cliente();
-                em.setCodigoCliente(rs.getInt(1));
-                em.setDPICliente(rs.getString(2));
-                em.setNombresCliente(rs.getString(3));
-                em.setDireccionCliente(rs.getString(4));
-                em.setEstado(rs.getString(5));
-                listaCliente.add(em);
+                Cliente cl = new Cliente();
+                cl.setCodigoCliente(rs.getInt(1));
+                cl.setDPICliente(rs.getString(2));
+                cl.setNombresCliente(rs.getString(3));
+                cl.setDireccionCliente(rs.getString(4));
+                cl.setEstado(rs.getString(5));
+                listaCliente.add(cl);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -44,16 +44,16 @@ public class ClienteDAO {
         return listaCliente;
     }
     
-    public int agregar(Cliente cli) {
+    public int agregar(Cliente cl) {
         String sql = "insert into Cliente (DPICliente, nombresCliente, direccionCliente, estado) values (?, ?, ?, ?)";
         try {
             con = cn.Conexion();
             ps = con.prepareStatement(sql);
 
-            ps.setString(1, cli.getDPICliente());
-            ps.setString(2, cli.getNombresCliente());
-            ps.setString(3, cli.getDireccionCliente());
-            ps.setString(4, cli.getEstado());
+            ps.setString(1, cl.getDPICliente());
+            ps.setString(2, cl.getNombresCliente());
+            ps.setString(3, cl.getDireccionCliente());
+            ps.setString(4, cl.getEstado());
             ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
@@ -62,40 +62,51 @@ public class ClienteDAO {
     }
     
     public Cliente listarCodigoCliente(int id) {
-        Cliente cli = new Cliente();
+        Cliente cl = new Cliente();
         String sql = "select * from Cliente where codigoCliente ="+id;
         try {
             con = cn.Conexion();
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                cli.setDPICliente(rs.getString(2));
-                cli.setNombresCliente(rs.getString(3));
-                cli.setDireccionCliente(rs.getString(4));
-                cli.setEstado(rs.getString(5));
+                cl.setDPICliente(rs.getString(2));
+                cl.setNombresCliente(rs.getString(3));
+                cl.setDireccionCliente(rs.getString(4));
+                cl.setEstado(rs.getString(5));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return cli;
+        return cl;
     }
     
-    public int actualizar(Cliente cli) {
+    public int actualizar(Cliente cl) {
         String sql = "update Cliente set DPICliente = ?, nombresCliente = ?, direccionCliente = ?, estado = ? where codigoCliente = ?";
 
         try {
             con = cn.Conexion();
             ps = con.prepareCall(sql);
 
-            ps.setString(1, cli.getDPICliente());
-            ps.setString(2, cli.getNombresCliente());
-            ps.setString(3, cli.getDireccionCliente());
-            ps.setString(4, cli.getEstado());
-            ps.setInt(6, cli.getCodigoCliente());
+            ps.setString(1, cl.getDPICliente());
+            ps.setString(2, cl.getNombresCliente());
+            ps.setString(3, cl.getDireccionCliente());
+            ps.setString(4, cl.getEstado());
+            ps.setInt(6, cl.getCodigoCliente());
             ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return resp;
+    }
+    
+    public void eliminar (int id) {
+        String sql = "delete from cliente where codigoCliente ="+id;
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
